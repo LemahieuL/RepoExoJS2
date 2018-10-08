2c09cfb7e302ecf1f856b30af8e27272825f7011
Luc <luc.lemahieu@laposte.net>
2018-10-08 09:11:39 +0200

    fini

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("Entrer votre nom :");
+var surname = prompt("Entrer votre prénom :");
+var town = prompt("Entrer votre ville :");
+alert("Nom : " + name + "\nPrénom : " + surname + "\nVille : " + town);
